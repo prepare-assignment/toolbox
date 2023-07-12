e13603d724653b25bc905a4b093c4dddc1dc1af6
Bonajo <m.bonajo@fontys.nl>
2023-07-12 11:05:58 +0200

    feat: addd more tests

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from typing import List
 
 import pytest
 
@@ -8,16 +9,21 @@ from prepare_toolbox.file import get_matching_files
 test_project_dir = os.path.join(Path(__file__).parent.absolute(), "testproject")
 
 
-def test_get_matching_files(monkeypatch: pytest.MonkeyPatch) -> None:
+@pytest.mark.parametrize(
+    "included, excluded, relative_to, recursive, expected",
+    [
+        (["**/*.java"], [], None, True, 5),
+        (["**/*.{java,xml}"], [], None, True, 6),
+        (["**/*.java"], [], None, False, 0),
+        (["**/*.java"], [], os.path.join("src", "main"), True, 3),
+        (["**/*.java"], ["**/GradeList.java"], None, False, 0),
+    ]
+)
+def test_get_matching_files(included: List[str], excluded: List[str], relative_to: str, recursive: bool, expected: int, monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.chdir(test_project_dir)
-    files = get_matching_files(["**/*.java"])
-    assert len(files) == 5
-
-
-def test_get_matching_files_excluded(monkeypatch: pytest.MonkeyPatch) -> None:
-    monkeypatch.chdir(test_project_dir)
-    files = get_matching_files(["**/*.java"], excluded=["**/GradeList.java"])
-    assert len(files) == 4
+    files = get_matching_files(included, excluded, relative_to=relative_to, recursive=recursive)
+    print(files)
+    assert len(files) == expected
 
 
 def test_get_matching_files_no_glob(monkeypatch: pytest.MonkeyPatch) -> None:
@@ -30,9 +36,3 @@ def test_get_matching_files_no_list(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.chdir(test_project_dir)
     files = get_matching_files("**/*.java")
     assert len(files) == 5
-
-
-def test_get_matching_files_relative(monkeypatch: pytest.MonkeyPatch) -> None:
-    monkeypatch.chdir(test_project_dir)
-    files = get_matching_files(["**/*.java"], relative_to=os.path.join("src", "main"))
-    assert len(files) == 3
