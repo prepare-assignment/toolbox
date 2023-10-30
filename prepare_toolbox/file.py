51f2bd732259f5983da0c29eec62801277da53fb
Bonajo <m.bonajo@fontys.nl>
2023-10-30 12:47:30 +0100

    feat: add support for directories outside pwd

--- prepare_toolbox/file.py
+++ prepare_toolbox/file.py
@@ -1,7 +1,7 @@
 import glob
 import os
 import sys
-from pathlib import Path
+from pathlib import Path, PurePath
 from typing import Union, List, Iterable, Set, Iterator
 
 from braceexpand import braceexpand
@@ -13,39 +13,57 @@ def __expand(path: str) -> Iterator[str]:
     return braceexpand(path)
 
 
-def __get_matching_files(globs: Union[str, List[str]], relative_to: Union[str, Path, None] = None,
-                         recursive: bool = True) -> Set[str]:
+def __get_matching_files(globs: Union[str, List[str]], relative_to: Union[str, Path, None],
+                         allow_outside_working_dir: bool, recursive: bool) -> Set[str]:
     if globs is None:
         raise ValueError("Cannot find matching files without included glob")
     if not isinstance(globs, list):
         globs = [globs]
     matched: Set[str] = set()
-    if relative_to is not None:
-        relative_to = Path(os.path.join(os.getcwd(), relative_to))
-    else:
-        relative_to = Path(os.getcwd())
     for g in globs:
         path = os.path.join(relative_to, g)
         for expanded in __expand(path):
             for file in glob.glob(expanded, recursive=recursive):
-                relative = Path(file).relative_to(relative_to)
-                matched.add(str(relative))
+                file = os.path.abspath(file)
+                if allow_outside_working_dir:
+                    matched.add(file)
+                else:
+                    relative = Path(file).relative_to(relative_to)
+                    matched.add(str(relative))
     return matched
 
 
 def get_matching_files(included: Union[str, List[str]], excluded: Union[str, List[str], None] = None,
-                       relative_to: Union[str, Path, None] = None, recursive: bool = True) -> List[str]:
+                       relative_to: Union[str, Path, None] = None, allow_outside_working_dir: bool = False,
+                       recursive: bool = True) -> List[str]:
     """
     Get files matching the included glob and not matching the excluded glob.
     :param included: Glob(s) that should be matched
     :param excluded: Glob(s) that should not be matched
     :param relative_to: Set relative path, the globs are matched relative to this path, defaults to pwd
+    :param allow_outside_working_dir: allow relative_to and the matched glob(s) to be outside the working directory
     :param recursive: Whether the glob should recurse directories
     :return List[str]: List of matched files (as posix strings)
-    :raises ValueError: If any of the matched files is outside the relative_to directory (or its children)
+    :raises ValueError: - If either relative_to is outside the working directory and allow_outside_working_dir is false.
+                        - If a matched glob is outside the relative_to directory and allow_outside_working_dir is false.
+                        - If relative_to is not a directory.
     """
-    matched_included = __get_matching_files(included, relative_to=relative_to, recursive=recursive)
+    if relative_to is not None:
+        if not os.path.isdir(relative_to):
+            raise ValueError(f"'relative_to' should be a directory")
+        # If relative is an absolute path it will overwrite the pwd
+        relative_to = Path(os.path.abspath(os.path.join(os.getcwd(), relative_to)))
+        if not allow_outside_working_dir:
+            # This will raise an ValueError if they are not relative
+            # As we support python3.8, we cannot use is_relative_to
+            relative_to.relative_to(os.getcwd())
+    else:
+        relative_to = Path(os.getcwd())
+
+    matched_included = __get_matching_files(included, relative_to=relative_to,
+                                            allow_outside_working_dir=allow_outside_working_dir, recursive=recursive)
     if excluded is not None:
-        matched_excluded = __get_matching_files(excluded, relative_to=relative_to, recursive=recursive)
+        matched_excluded = __get_matching_files(excluded, relative_to=relative_to,
+                                                allow_outside_working_dir=allow_outside_working_dir, recursive=recursive)
         matched_included -= matched_excluded
     return sorted(list(matched_included))
--- tests/test_file.py
+++ tests/test_file.py
@@ -10,30 +10,54 @@ test_project_dir = os.path.join(Path(__file__).parent.absolute(), "testproject")
 
 
 @pytest.mark.parametrize(
-    "included, excluded, relative_to, recursive, expected",
+    "included, excluded, relative_to, allow_outside, recursive, expected",
     [
-        (["**/*.java"], [], None, True, 5),
-        (["**/*.{java,xml}"], [], None, True, 6),
-        (["**/*.java"], [], None, False, 0),
-        (["**/*.java"], [], os.path.join("src", "main"), True, 3),
-        (["**/*.java"], ["**/GradeList.java"], None, False, 0),
-        (["src/main"], ["src/main"], None, False, 0),
+        (["**/*.java"], [], None, False, True, 5),
+        (["**/*.{java,xml}"], [], None, False, True, 6),
+        (["**/*.java"], [], None, False, False, 0),
+        (["**/*.java"], [], os.path.join("src", "main"), False, True, 3),
+        (["**/*.java"], ["**/GradeList.java"], None, False, False, 0),
+        (["src/main"], ["src/main"], None, False, False, 0),
+        (["**/*.java"], None, "..", True, True, 5),
     ]
 )
-def test_get_matching_files(included: List[str], excluded: List[str], relative_to: str, recursive: bool, expected: int, monkeypatch: pytest.MonkeyPatch) -> None:
+def test_get_matching_files(included: List[str], excluded: List[str], relative_to: str, allow_outside: bool,
+                            recursive: bool, expected: int, monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.chdir(test_project_dir)
-    files = get_matching_files(included, excluded, relative_to=relative_to, recursive=recursive)
-    print(files)
+    files = get_matching_files(included, excluded, relative_to=relative_to, allow_outside_working_dir=allow_outside,
+                               recursive=recursive)
     assert len(files) == expected
 
 
 def test_get_matching_files_no_glob(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.chdir(test_project_dir)
     with pytest.raises(ValueError):
-        files = get_matching_files(None)
+        get_matching_files(None)
 
 
 def test_get_matching_files_no_list(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.chdir(test_project_dir)
     files = get_matching_files("**/*.java")
     assert len(files) == 5
+
+
+def test_relative_to_no_dir(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.chdir(test_project_dir)
+    with pytest.raises(ValueError) as wrapped:
+        get_matching_files(["**/*.java"], None, relative_to="somefile.txt",
+                                   allow_outside_working_dir=True, recursive=True)
+    assert 'relative_to' in str(wrapped)
+
+
+def test_outside_not_allowed(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.chdir(test_project_dir)
+    with pytest.raises(ValueError):
+        get_matching_files(["**/*.java"], None, relative_to="..",
+                                   allow_outside_working_dir=False, recursive=True)
+
+
+def test_outside_not_allowed_glob(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.chdir(test_project_dir)
+    with pytest.raises(ValueError):
+        get_matching_files(["../**/*.py"], None, relative_to=None,
+                                   allow_outside_working_dir=False, recursive=True)
